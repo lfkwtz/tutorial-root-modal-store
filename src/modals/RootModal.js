c4ce1da9ff1503f83a2908d5422728e1f0f757b1
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2020-01-07 16:55:22 -0600

    step2

--- src/modals/RootModal.js
+++ src/modals/RootModal.js
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import {Modal, Button, View} from 'react-native';
 import {connect} from 'react-redux';
+import Error from './Error';
+import Success from './Success';
+
+const Modals = {
+  Error: Error,
+  Success: Success,
+};
 
 export class RootModal extends React.Component {
   render() {
@@ -12,7 +19,7 @@ export class RootModal extends React.Component {
             padding: 20,
             justifyContent: 'space-between',
           }}>
-          <View />
+          <Modals.Error />
           <Button onPress={() => {}} title="Close" color="blue" />
         </View>
       </Modal>
